515b90030a9ad0c82ddea6fbab0b33b8a792fed0
WhiteLeaf03 <2876202234@qq.com>
2023-07-10 15:19:56 +0800

    新增路由守卫

--- src/expand/router.js
+++ src/expand/router.js
@@ -1,4 +1,5 @@
 import * as VueRouter from 'vue-router'
+import {store} from './store'
 import Login from "../components/Login.vue";
 import System from "../components/System.vue";
 import User from "../components/pages/User.vue";
@@ -6,6 +7,8 @@ import Home from "../components/pages/Home.vue";
 import InterfaceInfo from "../components/pages/InterfaceInfo.vue";
 import InterfaceManage from "../components/pages/admin/InterfaceManage.vue";
 import Document from "../components/pages/Document.vue";
+import {ElMessage} from "element-plus";
+import {isEqual} from "./utils.js";
 
 const routes = [
     {
@@ -53,4 +56,28 @@ const routes = [
 export const router = VueRouter.createRouter({
     history: VueRouter.createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    const target = to.path
+
+    // 前往非登录页且未登录 跳转至登录页
+    if (isEqual(store.state.user, {nickname: 'unknown', avatar: 'unknown', gender: 'unknown', role: 'unknown', createTime: 'unknown'}) && target !== '/login') {
+        ElMessage.error("请先登录")
+        return next('/login')
+    }
+
+    // 已登录 前往管理员页 且有管理员权限
+    if (target.startsWith('/system/manage') && store.state.user.role === 'admin') {
+        return next()
+    }
+
+    // 已登录 前往管理员页 但无管理员权限 原地不动
+    if (target.startsWith('/system/manage') && store.state.user.role !== 'admin') {
+        ElMessage.error("权限不足")
+        return next(from.path)
+    }
+
+    // 其余情况，放行
+    next()
 })
